9b30068a0606ef2e9471c1cfe00588d952c61e3f
fm1227693-hub <fm1227693@gmail.com>
2026-07-22 21:27:44 +0500

    xatolik tuzatildi

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,21 @@ import Products from "./components/Products";
 import AboutUs from "./components/AboutUs";
 import Admin from "./components/Admin";
 import Register from "./components/Register";
-import StaticMentors from "./components/staticMentors";
-
+import StaticMentors from "./StaticMentors";
 export default function App() {
   return (
     <div className="dark:bg-[#090623]">
       <Navbar />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path='/stats' element={<Stats />} />
-        <Route path='/products' element={<Products />} />
-        <Route path='/about' element={<AboutUs />} />
-        <Route path='/enter' element={<Admin />} />
-        <Route path='/static' element={<StaticMentors />} />
-        <Route path='/register' element={<Register />} />
+        <Route path="/stats" element={<Stats />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/enter" element={<Admin />} />
+        <Route path="/static" element={<StaticMentors />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
       <Footer />
     </div>
-  )
+  );
 }
